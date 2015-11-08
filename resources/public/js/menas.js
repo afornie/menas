fe90f5f0bbabbd902fa1bef547c6d6d62d646ac2
afornie <antonio@splendo.com>
2015-11-08 16:22:22 +0100

    Improved login and layout a bit

--- resources/public/js/menas.js
+++ resources/public/js/menas.js
@@ -1,26 +1,35 @@
-//alert("menas.js was loaded");
-
 var app = angular.module("menasApp", []);
 
 app.controller("menasController", function($scope, $http) {
-
-  $scope.token = "";
-  $scope.addCategory = "opinion";
-  $scope.menas = [{"id" : "Web en construcción", "title" : "Cargando entradas", "body" : "Pulsa para actualizar"}]
+    $scope.tokenInput = "";
+    $scope.token = "";
+    $scope.menas = [];
     $scope.loadMenas = function () {
-        $http.get("api/menas")
-            .success(function(response) {
-                $scope.menas = response;
-            });
+      $http.get("api/menas")
+          .success(function(response) {
+              $scope.menas = response;
+          });
     };
     function getEmptyMena() {
         return {
-            title : "Type your title",
-            body : "Message..."
+            title : "",
+            body : ""
         }
     }
 
-    $scope.newMena = getEmptyMena();
+    $scope.login = function() {
+        $scope.token = $scope.tokenInput;
+        $scope.tokenInput="tokenUsed";
+    };
+    function resetNewMena() {
+        $scope.addCategorySupport = false;
+        $scope.addCategoryEvent = false;
+        $scope.addCategoryOpinion = false;
+        $scope.newMena = getEmptyMena();
+        $scope.addTags = "";
+    }
+    resetNewMena();
+
     $scope.submitMena = function () {
         var mena = {
             title : $scope.newMena.title,
@@ -28,11 +37,21 @@ app.controller("menasController", function($scope, $http) {
             tags : [],
             categories : []
         }
-        mena.categories.push($scope.addCategory)
+        mena.tags.push($scope.addTags)
+        if ($scope.addCategorySupport) {
+            mena.categories.push("support")
+        }
+        if ($scope.addCategoryOpinion) {
+            mena.categories.push("opinion")
+        }
+        if ($scope.addCategoryEvent) {
+            mena.categories.push("event")
+        }
         $http.post("api/menas?token="+$scope.token, mena)
             .success(function(response) {
-                newMena = getEmptyMena();
+                resetNewMena();
                 $scope.loadMenas();
             });
     };
+    $scope.loadMenas();
 });
